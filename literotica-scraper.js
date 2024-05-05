f3a5dafe46a78a379ebb505abf385e43533b4be5
RemigiuszWyciszkiewicz <remigiusz.wyciszkiewicz@gmail.com>
2024-05-05 12:53:53 +0200

    changed current

--- literotica-scraper.js
+++ literotica-scraper.js
@@ -195,12 +195,12 @@ const getStory = async ({ id, link, date }) => {
 
 async function start() {
   // await getLinks();
-  let offset = 38200;
+  let offset = 38800;
 
   let storyCount = 638033;
-  let currentStoryIndex = 38200;
+  let currentStoryIndex = 38800;
 
-  for (let index = 382; index < storyCount / 100; index++) {
+  for (let index = 388; index < storyCount / 100; index++) {
     const links = await getLink(offset);
 
     for (let index = 0; index < links.length; index++) {
@@ -239,6 +239,6 @@ async function sleep(millis) {
 
 // Story bdsm library count - 10849
 // literotica Link count - 638033
-// last session - 38200
+// last session - 38779
 
 // 25400 - 23:24
